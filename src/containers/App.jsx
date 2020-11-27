a7fac644d2b78184240f2aba5bfba029787697ea
tsmatus <tsmatus@uc.cl>
2020-11-27 00:22:38 -0300

    proyecto casi listo con detalles

---
+++ src/containers/App.jsx
@@ -0,0 +1,49 @@
+import React, {useState, useEffect} from 'react'
+import Header from '../components/Header'
+import Search from '../components/Search'
+import Categories from '../components/Categories'
+import Carousel from '../components/Carousel'
+import CarouselItem from '../components/CarouselItem'
+import '../assets/styles/App.scss'
+import Footer from '../components/Footer'
+import useInitialState from '../hooks/useInitialState'
+
+const API = 'http://localhost:3000/initialState'
+
+const App = () => {
+    const initialState = useInitialState(API)
+
+    return video.length === 0 ? <h1>Loading...</h1> :(
+        <div className="App">
+            <Header/>
+            <Search/>
+            {initialState.mylist.length > 0 &&
+            <Categories title = "Mi Lista">
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+
+            }
+            <Categories title = "Trents">
+                <Carousel>
+                    {initialState.trends.map(item =>
+                        <CarouselItem key={item.id} {...item}/>
+                    )}
+                </Carousel>
+            </Categories>
+
+            <Categories title = "Lo Mejor de Todo">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>
+            </Categories>
+
+            <Footer/>
+        </div>
+)
+}
+
+export default App
---
+++ src/hooks/useInitialState.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react'
+
+
+const [ videos, setVideos] = useState({mylist: [], trends: [], originals: [] })
+const useInitialState = (API) => {
+    const [ videos, setVideos] = useState({mylist: [], trends: [], originals: [] })
+
+    useEffect(() => {
+        fetch(API)
+        .then(response => response.json())
+        .then(data => setVideos(data))
+    }, [])
+}
+
+export default useInitialState;
