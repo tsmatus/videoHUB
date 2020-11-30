fbc3b49639c55fde95dcf2c6b568c77468a70d38
tsmatus <tsmatus@uc.cl>
2020-11-29 23:46:51 -0300

    corregido

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,15 +1,15 @@
 import {useState, useEffect} from 'react'
 
-
-const [ videos, setVideos] = useState({mylist: [], trends: [], originals: [] })
 const useInitialState = (API) => {
-  const [ videos, setVideos] = useState({mylist: [], trends: [], originals: [] })
+    const [ videos, setVideos] = useState({mylist: [], trends: [], originals: [] })
 
     useEffect(() => {
         fetch(API)
         .then(response => response.json())
         .then(data => setVideos(data))
-    }, [])
+    }, []); 
+
+    return videos
 }
 
 export default useInitialState;
