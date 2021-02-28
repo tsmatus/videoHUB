9a56e9fb53eee1f9ae9486d1ad09ed88322f0c11
tsmatus <tsmatus@uc.cl>
2021-02-28 09:49:58 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-<<<<<<< HEAD
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -59,66 +58,3 @@ module.exports = {
     new MiniCssExtractPlugin({ filename: 'assets/[name].css' }),
   ],
 };
-=======
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
-            },
-            {
-                test: /\.(s*)css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    'css-loader',
-                    'sass-loader'
-                
-                ]
-            },
-            {
-                test: /\.(png|gif|jpg)$/,
-                use: [
-                    {
-                        'loader': 'file-loader',
-                        options: {
-                            name: 'assets/[hash].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    devServer: {
-        historyApiFallback: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({filename: 'assets/[name].css'})
-    ]
-}
->>>>>>> feature/router-redux
